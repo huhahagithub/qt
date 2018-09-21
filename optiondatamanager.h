81114722416e1f8f478a24f2542f217294236619
huhahagithub <1298805382@qq.com>
2018-09-21 16:32:44 +0800

    option节点数据管理

---
+++ optiondatamanager.h
@@ -0,0 +1,16 @@
+#ifndef OPTIONDATAMANAGER_H
+#define OPTIONDATAMANAGER_H
+#include <QString>
+
+class optionDataManager
+{
+public:
+    optionDataManager(){}
+    QString type;
+    QString value;
+    QString id;
+    QString name;
+    QString unit;
+};
+
+#endif // OPTIONDATAMANAGER_H
